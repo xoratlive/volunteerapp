fea4db84daec74c010f88b00d2f08429b32176aa
xoratlive <xor@live.com>
2016-10-07 02:17:33 -0500

    changed listings to volunteer_opportunities in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
 
-  has_many :listings, dependent: :destroy
+  has_many :volunteer_opportunities, dependent: :destroy
 
 end
