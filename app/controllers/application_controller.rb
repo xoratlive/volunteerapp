12beeed1fdad3a6f3b85d8cb0e1e1e7af8548e95
xoratlive <xor@live.com>
2016-10-26 01:36:06 -0500

    added image to the account update sanitizer to fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :volunteer, :address, :biographical_information, :phone_number, :image ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :biographical_information, :phone_number ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :biographical_information, :phone_number, :image ])
   end
 
 
