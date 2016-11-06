58a554544f55d6b6ad52c4ce68af761021ea0f2b
xoratlive <xor@live.com>
2016-11-05 22:23:51 -0500

    try fix search bug

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,7 @@
 if Rails.env == 'production'
   ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
 end
+
+if ENV[‘BONSAI_URL’]
+     Searchkick.client = Elasticsearch::Client.new({url: ENV[‘BONSAI_URL’], logs: true})
+end
