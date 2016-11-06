7f70cb18600e01b224c27b6405968744e4e855cf
xoratlive <xor@live.com>
2016-11-06 05:44:47 -0600

    testing set day feature

--- app/controllers/volunteer_opportunities_controller.rb
+++ app/controllers/volunteer_opportunities_controller.rb
@@ -92,7 +92,7 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def volunteer_opportunity_params
-      params.require(:volunteer_opportunity).permit(:description, :Organization, :Address, :Phone, :Website)
+      params.require(:volunteer_opportunity).permit(:monday_morning, :description, :Organization, :Address, :Phone, :Website)
     end
     
     # Only let the creator of a volunteer opportunity edit or delete the opportunity.
