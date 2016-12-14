cf74ad0a4a0c26a0ab8df18f46abbdffc407b18a
xoratlive <xor@live.com>
2016-12-14 08:41:44 -0600

    disabled ability sign up as admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :volunteer, :admin ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :volunteer ])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :about1, :website, :address, :biographical_information, :phone_number, :image ])
   end
   def after_sign_in_path_for(resource)
